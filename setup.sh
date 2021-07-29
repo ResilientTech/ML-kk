2a8b758520bb9db6f8652ca59a6ec741367d8e60
Kal <65475069+ResilientTech@users.noreply.github.com>
2021-07-28 22:08:22 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mkdir -p ~/.streamlit/echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo "[general]
+email = \"kal108@gmail.com\"" > ~/.streamlit/config.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false" >> ~/.streamlit/config.toml
