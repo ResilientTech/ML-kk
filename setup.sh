5db8bda293015fca36098c61186e446da23ded2f
Kal <65475069+ResilientTech@users.noreply.github.com>
2021-07-28 22:23:08 -0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"kal108@gmail.com\"" > ~/.streamlit/config.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false" >> ~/.streamlit/config.toml
